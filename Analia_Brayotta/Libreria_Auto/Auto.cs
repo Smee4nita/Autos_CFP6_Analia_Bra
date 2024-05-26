3f521c818c64a57607cf572744a24651db828b06
Smee4nita <b.analia.laura@gmail.com>
2024-05-26 13:25:37 -0300

    Creación Bibliote Auto y consola Auto

---
+++ Analia_Brayotta/Libreria_Auto/Auto.cs
@@ -0,0 +1,104 @@
+﻿using System.Drawing;
+
+namespace Libreria_Auto
+{
+    public class Auto
+    {
+        private string marca;
+        private double cantidadCombustible;
+        private Color color;
+
+        public Auto(string marca, double cantidadCombustible, Color color)
+        {
+            this.marca = marca;
+            this.cantidadCombustible = cantidadCombustible;
+            this.color = color;
+        }
+        public Auto(string marca, string combustible, string color)
+        {
+            this.ConvertirStringEnCombustible(combustible);
+            this.ConvertirColorStringEnColor(color);
+            this.marca = marca;
+        }
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+        public double GetCantidadCombustible()
+        {
+            return cantidadCombustible;
+        }
+
+        public Color GetColor()
+        {
+            return color;
+        }
+
+
+        public void SetColor(Color color)
+        {
+            this.color = color;
+        }
+        public string AutoToString()
+        {
+            return $"la marca del auto es: {marca} , la cantidad de combustible que tiene es: {cantidadCombustible} y el color es: {color}";
+
+        }
+
+        public bool Avanzar(double km)
+        {   bool retorno = false;
+            double combustibleNecesario = km/10;
+            if (cantidadCombustible >0 && cantidadCombustible >= combustibleNecesario)
+            {
+               cantidadCombustible -= combustibleNecesario;
+                retorno = true;
+            }
+            return retorno;
+            
+        }
+
+        public bool ConvertirStringEnCombustible(string combustible)
+        {
+            bool retorno = false;
+            if (double.TryParse(combustible, out double combustibleValido))
+            {
+                this.cantidadCombustible = combustibleValido;
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public bool ConvertirColorStringEnColor(string nuevoColor)
+        {
+            bool retorno = true;
+
+            switch (nuevoColor)
+            {
+                case "rojo":
+                    this.color = Color.Red;
+                    break;
+                case "azul":
+                    this.color = Color.Blue;
+                    break;
+                case "blanco":
+                    this.color = Color.White;
+                    break;
+                case "verde":
+                    this.color = Color.Green;
+                    break;
+                default:
+                    retorno=false; break;
+
+            }
+            return retorno;
+
+        }
+
+        public override string ToString()
+        {
+            return this.AutoToString();
+        }
+
+    }
+}
---
+++ Analia_Brayotta/Testeo_Autos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using Libreria_Auto;
+
+namespace Testeo_Autos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto autoUno = new Auto("Honda", 60, Color.Beige);
+
+            Console.WriteLine(autoUno.ToString());
+
+            ///F. En un proyecto de consola crear un objeto del tipo Auto, mostrar todos su valores y dentro de un bucle repetitivo 
+            ///usar el metodo avanzar(), este debera repetirse hasta que el auto agote su combustible y se debera informar cuantas 
+            ///repeticiones logra realizar.
+
+            int repeticiones = 0;
+            while (autoUno.Avanzar(23))
+            {
+              repeticiones++;
+              
+            }
+
+            Console.WriteLine($"El auto logró realizar {repeticiones} repeticiones.");
+
+        }
+    }
+}
